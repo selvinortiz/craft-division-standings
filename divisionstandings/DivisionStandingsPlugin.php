05accd19f306f15d423b374a4be92415c4729ef0
Selvin Ortiz <selvin@selvin.co>
2017-03-29 13:44:00 -0500

    Completes initial working implementation

--- divisionstandings/DivisionStandingsPlugin.php
+++ divisionstandings/DivisionStandingsPlugin.php
@@ -3,6 +3,14 @@ namespace Craft;
 
 class DivisionStandingsPlugin extends BasePlugin
 {
+    protected $columnToFieldMapping = [
+        'name' => 'col1',
+        'wins' => 'col2',
+        'losses' => 'col3'
+    ];
+
+    protected $divisions = ['southDivision', 'northDivision'];
+
     public function init()
     {
         // Special binding for older version of PHP
@@ -11,7 +19,13 @@ class DivisionStandingsPlugin extends BasePlugin
         // Older version of PHP do not have access to $this within a callable
         craft()->templates->hook('generateDivisionStandings', function (&$context) use ($instance)
         {
-            return $instance->generateDivisionStandings($context['division']);
+            foreach ($instance->divisions as $division)
+            {
+                if (isset($context[ $division ]))
+                {
+                    $context[ $division ] = $instance->generateDivisionStandings($context[ $division ]);
+                }
+            }
         });
     }
 
@@ -22,7 +36,12 @@ class DivisionStandingsPlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '0.1.0';
+        return '0.2.0';
+    }
+
+    public function getSchemaVersion()
+    {
+        return '1.0.0';
     }
 
     public function getDescription()
@@ -47,14 +66,15 @@ class DivisionStandingsPlugin extends BasePlugin
         // Generated division standings
         $standings = [];
 
-        foreach ($division as $name => &$team)
+        foreach ($division as &$team)
         {
-            $wins = $this->getValueByKey($team, 'record.wins');
-            $losses = $this->getValueByKey($team, 'record.losses');
+            $name = $team[ $this->columnToFieldMapping['name'] ];
+            $wins = $team[ $this->columnToFieldMapping['wins'] ];
+            $losses = $team[ $this->columnToFieldMapping['losses'] ];
             $winning = $this->calculateWinningPercentage($wins, $losses);
 
-            $columns[$name] = $winning;
-            $standings[$name] = compact('name', 'wins', 'losses', 'winning');
+            $columns[ $this->textToHandle($name) ] = $winning;
+            $standings[ $this->textToHandle($name) ] = compact('name', 'wins', 'losses', 'winning');
         }
 
         array_multisort($columns, SORT_DESC, SORT_NUMERIC, $standings);
@@ -64,25 +84,20 @@ class DivisionStandingsPlugin extends BasePlugin
 
     public function addGamesBackColumn(array $standings)
     {
-        $divisionCopy = $standings;
-        
+        $standingsCopy = $standings;
+        $standingsLeader = array_shift($standingsCopy);
+
         foreach ($standings as &$team)
         {
-            $conferenceCopy = $conference;
-            $conferenceLeader = array_shift($conferenceCopy);
-
-            foreach ($conference as &$team)
-            {
-                $team['gamesBack'] = (($conferenceLeader['wins'] - $team['wins']) + ($team['losses'] - $conferenceLeader['losses'])) / 2;
-            }
-
-            $conferenceLeader['games'] = 0;
+            $team['gamesBack'] = (($standingsLeader['wins'] - $team['wins']) + ($team['losses'] - $standingsLeader['losses'])) / 2;
         }
+
+        // $standingsLeader['games'] = 0;
 
         return $standings;
 	}
 
-    public function getWinningPercentage($wins, $losses)
+    public function calculateWinningPercentage($wins, $losses)
     {
         return round($wins / ($wins + $losses), 3);
     }
@@ -112,5 +127,16 @@ class DivisionStandingsPlugin extends BasePlugin
         }
 
         return array_key_exists($key, $arr) ? $arr[$key] : $default;
+    }
+
+    public function textToHandle($text) {
+        $words = explode(' ', $text);
+        $firstWord = mb_strtolower(array_shift($words));
+        $words = array_map(function ($word)
+        {
+            return ucfirst($word);
+        }, $words);
+
+        return $firstWord.implode('', $words);
     }
 }
